6b9107feb6b1f25b0c4669a798250a0efd830c57
Etiksha Jain <jainetiksha@gmail.com>
2021-10-24 00:09:30 +0530

    text summary, preview, lowercase

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -3,10 +3,15 @@ import PropTypes from 'prop-types'
 
 export default function TextForm(props) {
 
-    const [text,setText]=useState("Enter text here");
+    const [text,setText]=useState("");
 
     function handleUpClick(){
         let newText=text.toUpperCase();
+        setText(newText);
+    }
+
+    function handleDownClick(){
+        let newText=text.toLowerCase();
         setText(newText);
     }
 
@@ -16,11 +21,25 @@ export default function TextForm(props) {
 
     return(
         <>
-        <div div className="mb-3">
-            <h1>{props.heading}</h1>
-            <textarea className="form-control" id="my-box" rows="10" onChange={handleOnChange} value={text}></textarea>
+        <div className="container">
+            <h2>{props.heading}</h2>
+            <div div className="mb-3">
+                <textarea className="form-control" id="my-box" rows="10" onChange={handleOnChange} value={text} placeholder="Enter Text"></textarea>
+            </div>
+            <button className="btn btn-primary mx-2" onClick={handleUpClick}>Convert to UpperCase</button>
+            <button className="btn btn-primary mx-2" onClick={handleDownClick}>Convert to LowerCase</button>
         </div>
-        <button className="btn btn-primary" onClick={handleUpClick}>Convert to UpperCase</button>
+
+        <div className="container my-2">
+            <h2>Text Summary:</h2>
+            <ul>
+                <li>Words: {text.split(" ").length}</li>
+                <li>Characters: {text.length}</li>
+                <li>Minute read: {text.split(" ").length * 0.008}</li>
+            </ul>
+            <h2>Preview:</h2>
+            <p>{text}</p>
+        </div>
         </>
     )
 }
